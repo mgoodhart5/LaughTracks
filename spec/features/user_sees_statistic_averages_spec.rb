087aa8131aab5352c8cdde10e9639947ce80e569
Mary Goodhart <marya.grace5@gmail.com>
2018-11-29 13:17:22 -0700

    fixes spec test

--- spec/features/user_sees_statistic_averages_spec.rb
+++ spec/features/user_sees_statistic_averages_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "As a user" do
   describe "When I visit /comedians" do
-    it "should see an average of the comedians ages" do
+    it "should see unique hometowns and averages of the comedians ages, special run_times" do
       Comedian.create(name: "name_1", hometown: "hometown_1", age: 1)
       Comedian.create(name: "name_2", hometown: "hometown_2", age:2)
       Comedian.create(name: "name_3", hometown: "hometown_1", age: 3)
@@ -18,7 +18,7 @@ RSpec.describe "As a user" do
         expect(page).to have_content("Average Run Time:")
         expect(page).to have_content(Special.average_run_time)
         expect(page).to have_content("Hailing From These Cities:")
-        expect(page).to have_content(Comedian.unique_list_of_hometowns)
+        expect(page).to have_content("#{Comedian.unique_list_of_hometowns.join(', ')}")
       end
     end
   end
