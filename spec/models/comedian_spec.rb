c8c02f48891595f9bb8f86489dae059c1f51c2a4
Mary Goodhart <marya.grace5@gmail.com>
2018-11-29 10:41:31 -0700

    adds passing model test and comedian method

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Comedian do
       end
     end
   end
+
+  describe 'Class Methods' do
+    describe '.average_age' do
+      it 'returns average age for all comedians' do
+        Comedian.create(name: "name_1", hometown: "hometown_1", age: 1)
+        Comedian.create(name: "name_2", hometown: "hometown_2", age:2)
+
+        expect(Comedian.average_age).to eq(1.5)
+      end
+    end
+  end
 end
